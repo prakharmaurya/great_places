78d228ba15c74901c24ff232a7533ec81681415e
Excel <000.excel@gmail.com>
2020-06-06 09:03:26 +0530

    Add: Model Screens Added

---
+++ lib/screens/add_place_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AddPlaceScreen extends StatelessWidget {
+  static const routeName = 'add-place';
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Add Place'),
+    );
+  }
+}
---
+++ lib/screens/place_details_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PlaceDetailsScreen extends StatelessWidget {
+  static const routeName = '/place-details';
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        'Details',
+      ),
+    );
+  }
+}
