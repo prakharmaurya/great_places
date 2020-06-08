c3b063d45d1ac4100872b966b9b445e3d7e61b12
Excel <000.excel@gmail.com>
2020-06-08 10:24:08 +0530

    Add: Form added to pic place

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,11 +1,60 @@
 import 'package:flutter/material.dart';
 
-class AddPlaceScreen extends StatelessWidget {
+import '../widgets/image_input.dart';
+
+class AddPlaceScreen extends StatefulWidget {
   static const routeName = 'add-place';
+
+  @override
+  _AddPlaceScreenState createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Add Place'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Add a new place',
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Title',
+                      ),
+                      controller: _titleController,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          RaisedButton.icon(
+            icon: Icon(Icons.add),
+            label: Text('Add place'),
+            onPressed: () {},
+            elevation: 0,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            color: Theme.of(context).accentColor,
+          )
+        ],
+      ),
     );
   }
 }
