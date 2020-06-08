715fcb03e59aada912b3170e2eb9d0132cc5deb2
Excel <000.excel@gmail.com>
2020-06-08 14:56:29 +0530

    Add: Add Place form submit done

--- lib/provider/places_provider.dart
+++ lib/provider/places_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 
 import '../models/place.dart';
@@ -7,5 +9,19 @@ class PlacesProvider with ChangeNotifier {
 
   List<Place> get items {
     return [..._places];
+  }
+
+  void addPlace(
+    String pickedTitle,
+    File pickedImage,
+  ) {
+    final newPlace = Place(
+      id: DateTime.now().toString(),
+      title: pickedTitle,
+      image: pickedImage,
+      location: null,
+    );
+    _places.add(newPlace);
+    notifyListeners();
   }
 }
--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/image_input.dart';
+import '../provider/places_provider.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = 'add-place';
@@ -17,6 +19,15 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
 
   void _selectImage(File pickedImage) {
     _pickedImage = pickedImage;
+  }
+
+  void _savePlace() {
+    if (_titleController.text.isEmpty || _pickedImage == null) {
+      return;
+    }
+    Provider.of<PlacesProvider>(context, listen: false)
+        .addPlace(_titleController.text, _pickedImage);
+    Navigator.of(context).pop();
   }
 
   @override
@@ -55,7 +66,7 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
           RaisedButton.icon(
             icon: Icon(Icons.add),
             label: Text('Add place'),
-            onPressed: () {},
+            onPressed: _savePlace,
             elevation: 0,
             materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
             color: Theme.of(context).accentColor,
