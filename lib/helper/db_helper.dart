594c12e14be5185a46a30b1d832af1e2beb4ffde
Excel <000.excel@gmail.com>
2020-06-22 13:52:32 +0530

    Add: Insert and get data added

--- lib/helper/db_helper.dart
+++ lib/helper/db_helper.dart
@@ -1,13 +1,11 @@
 import 'package:sqflite/sqflite.dart' as sql;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqlite_api.dart';
 
 class DBHelper {
-  static Future<void> insert(
-    String table,
-    Map<String, Object> data,
-  ) async {
+  static Future<Database> database() async {
     final dbPath = await sql.getDatabasesPath();
-    sql.openDatabase(
+    return sql.openDatabase(
       path.join(dbPath, 'places.db'),
       onCreate: (db, version) {
         return db.execute(
@@ -15,5 +13,22 @@ class DBHelper {
       },
       version: 1,
     );
+  }
+
+  static Future<void> insert(
+    String table,
+    Map<String, Object> data,
+  ) async {
+    final db = await DBHelper.database();
+    await db.insert(
+      table,
+      data,
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  static Future<List<Map<String, dynamic>>> getData(String table) async {
+    final db = await DBHelper.database();
+    return db.query(table);
   }
 }
