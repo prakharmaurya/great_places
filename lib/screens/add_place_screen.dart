84357781d534f0a582ca7cb489dc2fe0148ffd6b
Excel <000.excel@gmail.com>
2020-06-22 16:55:07 +0530

    Add: Location input added to add location screen

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 
 import '../widgets/image_input.dart';
 import '../provider/places_provider.dart';
+import '../widgets/location_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = 'add-place';
@@ -58,6 +59,8 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                       height: 10,
                     ),
                     ImageInput(_selectImage),
+                    SizedBox(height: 10),
+                    LocationInput()
                   ],
                 ),
               ),
