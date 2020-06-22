a79eea1c6724e287baabc2d5d56009b9cc73f4af
Excel <000.excel@gmail.com>
2020-06-22 16:33:23 +0530

    Add: added fetching DB a start

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -19,26 +19,35 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Consumer<PlacesProvider>(
-        child: Center(
-          child: const Text('Got no places yet!!!'),
-        ),
-        builder: (ctx, places, ch) => places.items.length <= 0
-            ? ch
-            : ListView.builder(
-                itemBuilder: (c, i) => ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: FileImage(
-                      places.items[i].image,
+      body: FutureBuilder(
+        future: Provider.of<PlacesProvider>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, snapshot) =>
+            snapshot.connectionState == ConnectionState.waiting
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Consumer<PlacesProvider>(
+                    child: Center(
+                      child: const Text('Got no places yet!!!'),
                     ),
+                    builder: (ctx, places, ch) => places.items.length <= 0
+                        ? ch
+                        : ListView.builder(
+                            itemBuilder: (c, i) => ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage: FileImage(
+                                  places.items[i].image,
+                                ),
+                              ),
+                              title: Text(
+                                places.items[i].title,
+                              ),
+                              onTap: () {},
+                            ),
+                            itemCount: places.items.length,
+                          ),
                   ),
-                  title: Text(
-                    places.items[i].title,
-                  ),
-                  onTap: () {},
-                ),
-                itemCount: places.items.length,
-              ),
       ),
     );
   }
